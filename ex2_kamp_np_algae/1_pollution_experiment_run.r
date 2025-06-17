e34af72af7e3d75ce2ab3b30995684a9112b6b9a
Lauren Talluto <lauren.talluto@uibk.ac.at>
2025-06-17 16:23:30 +0200

    some missing files

--- ex2_kamp_np_algae/1_pollution_experiment_run.r
+++ ex2_kamp_np_algae/1_pollution_experiment_run.r
@@ -2,28 +2,18 @@ library(flume)
 library(ggplot2)
 library(data.table)
 
-data(algae)
+# data(algae) # not needed as of 0.6.0.9009
 data(kamp)
-
+data(kamp_raw) # for the raw niche params, better customisation than just using the mc in data(kamp)
 options(mc.cores = 4)
 
-## notes march 19
-# 1. eliminated dispersal from outside the network. This reduces diversity on average in polluted nodes
-# but the effect is small and noisy
-# 2. cut niche breadth in half. no effect
-# 3. Increase extinction
-# finally increasing extinction to insane levels had an effect. maybe dynamics are too slow? 
-# need to investigate probs at each time step. I also really think an inverse-gaussian ext curve
-# would be helpful.
+dispersal_params = list(alpha = 0.5, beta = 0.05)
 
-# dispersal_params = list(alpha = 0.05, beta = 0.1)
-dispersal_params = list(alpha = 0.01, beta = 0.05)
-
-# set up metacommunity
 n_args = with(algae,
-	list(location = niche_params$location, breadth = niche_params$breadth, 
-	scale = list(col = 1e-5 * niche_params$scale, ext = 1e-6 * niche_params$scale), efun = "inverse_gaussian",
-	r_use = c(1e-3, 5e-4), ratio = matrix(1:2, ncol=2)))
+			  list(location = niche_params$location, breadth = 0.5*niche_params$breadth, 
+			  	 scale = list(col = 10* 1e-5 * niche_params$scale, ext = 1e-7), efun = "inverse_gaussian",
+			  	 r_use = c(1e-3, 5e-4), ratio = matrix(1:2, ncol=2)))
+
 mc = metacommunity(nsp = nrow(algae$niche_params), nr = 2, niches = niches_custom, 
 	dispersal = dispersal_custom, sp_names = rownames(algae$niche_params), r_names = c("N", "P"), 
 	niche_args = n_args, dispersal_args = dispersal_params, comp_scale = 1e-4)
@@ -32,32 +22,22 @@ mc = metacommunity(nsp = nrow(algae$niche_params), nr = 2, niches = niches_custo
 # Add boundary conditions to keep species from extinction. 
 # It assumes that dispersal from outside the network
 # is equal to 1/4 of a site, for each site, using both active and passive dispersal
-# qmat = matrix(boundary(kamp, "Q"), 
-# 	nrow = length(kamp), 
-# 	ncol = attr(mc, "n_species"))
-# bflux = sweep(qmat, 2, dispersal_params(mc)$beta, `*`)
-# bflux[bflux < 0] = 0
-# bnd_sp = 0.25 * sweep(bflux, 2, dispersal_params(mc)$alpha, `+`)
-
-## 19 march, try drastically reducing this
-qmat = matrix(boundary(kamp, "Q"), 
-	nrow = length(kamp), 
+qmat = matrix(boundary(kamp, "Q"),
+	nrow = length(kamp),
 	ncol = attr(mc, "n_species"))
 bflux = sweep(qmat, 2, dispersal_params(mc)$beta, `*`)
 bflux[bflux < 0] = 0
-bnd_sp = 0 * sweep(bflux, 2, dispersal_params(mc)$alpha, `+`)
-
-
+# bnd_sp = 0.25 * sweep(bflux, 2, dispersal_params(mc)$alpha, `+`)
+## 19 march, try drastically reducing this
+# bnd_sp = 0 * sweep(bflux, 2, dispersal_params(mc)$alpha, `+`)
+## 30 April try bringing it back, but 1/8 of a site
+bnd_sp = 0.125 * sweep(bflux, 2, dispersal_params(mc)$alpha, `+`)
 
 # set up the model
 mod = flume(mc, kamp, algae$sp0, algae$r0, spb = bnd_sp)
 
 
-
 ## simple experiment: try pollution next to control to see if there is any effect
-
-# note march 19:
-# trying a run with only the most extreme to see if we can even create an effect
 
 # simulate a point source of pollution by adding a point source of nitrogen
 # EU water quality standards limit 50 mg/L NO3 (which is 11.3 mg/L of N from NO3)
@@ -68,21 +48,18 @@ mod = flume(mc, kamp, algae$sp0, algae$r0, spb = bnd_sp)
 # this one has lots of nice info, with some limits. 30 mg (total N) is the limit for discharges
 # https://www.sciencedirect.com/science/article/pii/S0960852408002915
 # here we choose three concentrations, equal to 0.5x, 1x, and 2x the maximum
-# poll_concentrations = c(15, 30, 60)
-poll_concentrations = c(6000)
+poll_concentrations = c(15, 30, 60)
 
 # Choose three different nodes to try and the source of pollution
-# pnodes = c(15, 34, 41)
-pnodes = c(41)
+pnodes = c(15, 34, 41)
 
 # Will need to ensure that the extra discharge propagates downstream of the given node
 # These are the nodes that are downstream of our polluted nodes
-# dsnodes = list("n15" = c(13, 14, 1:9), "n34" = c(31:33, 25:29, 1:4), "n41" = c(38:40, 25:28, 1:4))
-dsnodes = list("n41" = c(38:40, 25:28, 1:4))
+stop("TODO: replace this with the new add_discharge() interface")
+dsnodes = list("n15" = c(13, 14, 1:9), "n34" = c(31:33, 25:29, 1:4), "n41" = c(38:40, 25:28, 1:4))
 
 # Choose three different discharges for pollution: 50, 100, and 200 L/sec
-# poll_discharges = c(0.05, 0.1, 0.2)
-poll_discharges = c(0.2)
+poll_discharges = c(0.05, 0.1, 0.2)
 
 
 # Phase 1: pre-pollution warm-up
@@ -92,7 +69,7 @@ phase1_len = 100
 phase2_len = 130
 
 # Number of replicated networks within a given flume
-reps = 8
+reps = 20
 
 outdir = "ex2_kamp_np_algae/results"
 
@@ -102,6 +79,7 @@ model_settings = list(
 )
 
 saveRDS(model_settings, "model_settings.rds")
+
 
 j = 1
 maxj = length(pnodes) * length(poll_discharges) * length(poll_concentrations)
@@ -121,6 +99,7 @@ for(p_node in pnodes) {
 				boundary(res_pol$networks[[i]], "resources")[p_node, 1] = p_conc
 
 				# add discharge to the polluted and all downstream nodes
+				stop("TODO: replace this with the new add_discharge() interface")
 				discharge(res_pol$networks[[i]])[c(p_node, p_ds), 1] = 
 					discharge(res_pol$networks[[i]])[c(p_node, p_ds), 1] + p_discharge
 			}
@@ -134,7 +113,43 @@ for(p_node in pnodes) {
 	}
 }
 
+
+
 # comparison with no pollution
 res_nopol = run_simulation(mod, phase1_len + phase2_len, reps = reps)
 saveRDS(res_nopol, file.path(outdir, "no_pollution.rds"))
 
+
+# river network maps
+# TODO: add an option to reduce the base size of the edge width
+
+pol_summary = summarise(res_pol, stat = "richness", quantile = 0.5, t_steps = 201:231)
+pol_summary = pol_summary[, .(richness = mean(richness)), by = .(reach)]
+npol_summary = summarise(res_nopol, stat = "richness", quantile = 0.5, t_steps = 201:231)
+npol_summary = npol_summary[, .(richness = mean(richness)), by = .(reach)]
+
+par(mfrow = c(2,2), mar = c(0,0,0,0), oma = c(0,4,0,0))
+i = match(attr(res_pol$networks[[1]],"names_sites"), pol_summary$reach)
+smax = max(c(pol_summary$richness, npol_summary$richness))
+plot.river_network(res_pol$networks[[1]], attribute = pol_summary$richness[i],
+					legend_args = list(title = "richness"), zlim = c(0, smax))
+mtext("Polluted", side = 2)
+zmax = max(state(res_pol$networks[[1]], "resources")[,'N'])
+plot.river_network(res_pol$networks[[1]], attribute = "resource", v_palette = "OrRd", zlim = c(0,zmax), log = TRUE)
+
+i = match(attr(res_nopol$networks[[1]],"names_sites"), npol_summary$reach)
+plot.river_network(res_nopol$networks[[1]], attribute = npol_summary$richness[i],
+					legend_args = list(title = "richness"), zlim = c(0, smax))
+mtext("Not Polluted", side = 2)
+plot.river_network(res_nopol$networks[[1]], attribute = "resource", v_palette = "OrRd", zlim = c(0,zmax), log = TRUE)
+
+
+
+
+# 
+# hist(as.vector(col_prob(mod$metacom, mod$networks[[1]], mod$dt)), main = "Col prob, default")
+# hist(as.vector(ext_prob(mod$metacom, mod$networks[[1]], mod$dt)), main = "Ext prob, default")
+# 
+# hist(as.vector(col_prob(res_pol$metacom, res_pol$networks[[1]], res_pol$dt)), main = "Col prob, polluted")
+# hist(as.vector(ext_prob(res_pol$metacom, res_pol$networks[[1]], res_pol$dt)), main = "Ext prob, polluted")
+# 
--- ex2_kamp_np_algae/2_process_results.R
+++ ex2_kamp_np_algae/2_process_results.R
@@ -1,10 +1,10 @@
 library(data.table)
 library(flume)
 library(ggplot2)
-res_files = list.files("results")
+res_files = list.files("ex2_kamp_np_algae/results")
+data(kamp)
 
-process_results = function(f) {
-	rr = readRDS(file.path("results", f))
+parse_fname = function(f) {
 	regex = "poll_conc(\\d+)_Q(\\d+)_n(\\d+)\\.rds"
 	if(f == "no_pollution.rds") {
 		conc = 0
@@ -15,88 +15,230 @@ process_results = function(f) {
 		Q = as.integer(sub(regex, "\\2", f))
 		node = as.integer(sub(regex, "\\3", f))
 	}
+	list(conc = conc, Q = Q, node = node)
+}
 
-	summ1 = summarise(rr, "reach", stat = "richness", t_steps = 131:161)
-	summ1$time_steps = "131:161"
-	summ2 = summarise(rr, "reach", stat = "richness", t_steps = 201:231)
-	summ2$time_steps = "201:231"
+process_results = function(f) {
+	rr = readRDS(file.path("ex2_kamp_np_algae/results", f))
+	params = parse_fname(f)
 
-	res = rbind(summ1, summ2)
-	res$concentration = conc
-	res$Q = Q
-	res$pollution_reach = node
+	# initial exploration showed this one was not needed
+	# summ1 = summarise(rr, "reach", stat = "richness", t_steps = 131:161)
+	# summ1$time_steps = "131:161"
+	summ1 = summarise(rr, "reach", stat = "richness", t_steps = 202:231)
+
+	summ2 = summarise(rr, c("reach", "resources"), stat = "concentration", t_steps = 202:231)
+	summ2 = summ2[resources == "N"]
+	summ2 = summ2[,resources := NULL]
+	colnames(summ2) = c("reach", "N", "N_lo", "N_hi")
+	
+	res = merge(summ1, summ2, "reach")
+	res$pollution_conc = params$conc
+	res$pollution_Q = params$Q
+	res$pollution_reach = params$node
 
 	res
 }
 
-sim_results = rbindlist(parallel::mclapply(res_files, process_results, mc.cores = 8))
+sim_results = rbindlist(parallel::mclapply(res_files, process_results, mc.cores = 4))
 
-## first figure: compare mean species richness in polluted nodes at different pollution discharge and concentration
+### Figure 1: richness map, using the highest concentration and a single pollution reach, days 202:231
+### TODO: for sup mat, create maps of all scenarios with uncertainties
+r_map_pol = sim_results[pollution_conc == 60 & pollution_Q == 200 & pollution_reach == 41, 
+						.(reach, richness, N)]
+r_map_npol = sim_results[pollution_conc == 0, .(reach, richness, N)]
 
-fig1_res = sim_results[(is.na(pollution_reach) & reach %in% unique(pollution_reach)) | pollution_reach == reach]
-fig1_res$polluted = !is.na(fig1_res$pollution_reach)
-ggplot(fig1_res, aes(x = concentration, y = richness, color = factor(polluted))) + 
-	geom_point() + 
-	geom_errorbar(aes(x = concentration, ymin = richness_lo, ymax = richness_hi)) +
-	facet_grid(reach ~ time_steps)
-# clear story here - no effect on species richness
+# reorder the rows to match the order in the river network
+r_map_pol = r_map_pol[match(attr(kamp, "names_sites"), r_map_pol$reach)]
+r_map_npol = r_map_npol[match(attr(kamp, "names_sites"), r_map_npol$reach)]
 
-## second figure: show change in richness compared with control at the same time period as a line graph moving downstream
-		## does richness converge on control?
+## TODO: this should be replaced with plot.flume() methods now
 
-
-model_settings = readRDS("model_settings.rds")
-
-# replicate the control across different "treatments" for comparison
-# note - should prob re-doc control where we *actually* change discharge with no poll
-fig2_ctl = rbind(sim_results[is.na(pollution_reach)], 
-	sim_results[is.na(pollution_reach)], sim_results[is.na(pollution_reach)])
-fig2_ctl$Q = rep(c(50, 100, 200), each = sum(is.na(sim_results$pollution_reach)))
-
-xorder_15 = c(21, 20, 19, 18, 17, 15, 14, 13, 9, 8, 7, 6, 5, 4, 3, 2, 1)
-fig2_res_15 = rbind(
-	sim_results[reach %in% xorder_15 & pollution_reach == 15],
-	fig2_ctl[reach %in% xorder_15])
-fig2_res_15$pollution_reach = 15
-fig2_res_15$xpos = match(fig2_res_15$reach, xorder_15)
-
-xorder_34 = c(35, 36, 34, 31, 32, 33, 29, 28, 26, 27, 25, 4, 3, 2, 1)
-fig2_res_34 = rbind(
-	sim_results[reach %in% xorder_34 & pollution_reach == 34],
-	fig2_ctl[reach %in% xorder_34])
-fig2_res_34$pollution_reach = 34
-fig2_res_34$xpos = match(fig2_res_34$reach, xorder_34)
-
-xorder_41 = c(42, 43, 41, 40, 39, 38, 28, 26, 27, 25, 4, 3, 2, 1)
-fig2_res_41 = rbind(
-	sim_results[reach %in% xorder_41 & pollution_reach == 41],
-	fig2_ctl[reach %in% xorder_41])
-fig2_res_41$pollution_reach = 41
-fig2_res_41$xpos = match(fig2_res_41$reach, xorder_41)
-
-fig2_res = rbind(fig2_res_15, fig2_res_34, fig2_res_41)
-fig2_res = fig2_res[time_steps == "131:161"]
-fig2_res[, is_pnode := (reach == pollution_reach)]
-
-ggplot(fig2_res, aes(x = xpos, y = richness, colour = as.factor(concentration), pch = is_pnode)) + 
-	geom_point() + geom_line() + geom_label(aes(label = reach), nudge_x = -0.5, nudge_y = 0.5, size = 3) + 
-	facet_grid(pollution_reach ~ factor(Q))
-
-# the control is identical to pollution treatments
-# at this point, need to consider why this is
-# two hypotheses - maybe dispersal from outside is driving this, or maybe niche-based dynamics too
-#    weak. might need a U-shaped extinction-curve to balance this. Can also make col niche narrower
+atval = -1
+line = 1.2
+pdf(width=20, height = 20, file = "ex2_kamp_np_algae/figures/fig_ex2_map.pdf", pointsize=24)
+par(mfrow=c(2,2), mar = c(0,1,3,0))
+plot(kamp, attribute = r_map_pol$richness, legend_args = list(title = "Species\nrichness"), v_palette = "Purples")
+mtext("A", side = 3, at = atval, line = line)
+plot(kamp, attribute = r_map_pol$N, legend_args = list(title = "[N] (mg/L)"), v_palette = "OrRd")
+mtext("B", side = 3, at = atval, line = line)
+plot(kamp, attribute = r_map_npol$richness, legend_args = list(title = "Species\nrichness"), v_palette = "Purples")
+mtext("C", side = 3, at = atval, line = line)
+plot(kamp, attribute = r_map_npol$N, legend_args = list(title = "[N] (mg/L)"), v_palette = "OrRd")
+mtext("D", side = 3, at = atval, line = line)
+dev.off()
 
 
 
-# check beta diversity for above stories
-## look at EF and the BEF relationship of polluted and non-polluted reaches and their downstream catchments
+
+### Figure 2: compared across all treatments in polluted reaches
+
+lags = list(r41 = data.table(
+	reach = as.character(c(1,  2,  3, 4, 25, 26, 27, 28, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 51, 52)),
+	lag =   c(11, 10, 9, 8, 7,  5,  6,  4,  3,  2,  1,  0,  -2, -1, -1, -2, -3, -3, -2, -2)
+), r15 = data.table(
+	reach = as.character(c(1,  2, 3, 5, 6, 7, 8, 9, 13, 14, 15, 16, 17)),
+	lag =   c(10, 9, 8, 7, 6, 5, 4, 3, 1,  2,  0, -1, -1)
+), r34 = data.table(
+	reach = as.character(c(1,  2,  3,  4, 25, 26, 27, 28, 29, 31, 32, 33, 34, 35, 36, 37)),
+	lag =   c(12, 11, 10, 9, 8,  6,  7,  5,  4,  1,  2,  3,  0,  -2, -1, -1)
+))
+
+# duplicate the control across each pollution reach
+res_lplot = rbindlist(lapply(c(41, 15, 34), \(r) {
+	res = sim_results[pollution_reach == r | is.na(pollution_reach)]
+	res$pollution_reach = r
+	merge(res, lags[[paste0('r', r)]], by = "reach", all.x = TRUE)
+}))
+res_lplot$label = NA
+res_lplot$label[res_lplot$reach == "4"] = "4"
 
 
-gridExtra::grid.arrange(
-	plot(res_pol),
-	plot(res_pol, "ef"),
-	plot(res_nopol),
-	plot(res_nopol, "ef"),
-	ncol = 2
-)
+pdf(width=20, height = 20, file = "ex2_kamp_np_algae/figures/fig_ex2_rich_all.pdf")
+ggplot(res_lplot, aes(x = lag, y = richness, colour = factor(pollution_conc))) + geom_line() + 
+	geom_errorbar(aes(ymin = richness_lo, ymax = richness_hi), width = 0.5) + 
+	facet_grid(rows = vars(pollution_reach), cols = vars(pollution_Q)) + 
+	geom_label(aes(y = 40, label = label), colour = "black", size = 8, label.padding = unit(0.1, "lines")) + 
+	geom_vline(xintercept = 0, lty=2, colour = "#777777") + 
+	ylim(0, 45) + theme_light(base_size=24) + xlab("Lag (reaches from pollution source)") + ylab("Species Richness") +
+	scale_colour_manual("[N] (mg/L)", values = c("#fdd49e", "#fc8d59", "#d7301f", "#7f0000"))
+dev.off()
+
+pdf(width=10, height = 20, file = "ex2_kamp_np_algae/figures/fig_ex2_rich_100.pdf", pointsize=36)
+ggplot(res_lplot[pollution_Q == 100 | pollution_Q == 0], aes(x = lag, y = richness, colour = factor(pollution_conc))) + geom_line() + 
+	geom_errorbar(aes(ymin = richness_lo, ymax = richness_hi), width = 0.5) + 
+	facet_grid(rows = vars(pollution_reach)) + 
+	geom_label(aes(y = 40, label = label), colour = "black", size = 8, label.padding = unit(0.1, "lines")) + 
+	geom_vline(xintercept = 0, lty=2, colour = "#777777") + 
+	ylim(0, 45) + theme_light(base_size=24) + xlab("Lag (reaches from pollution source)") + ylab("Species Richness") +
+	scale_colour_manual("[N] (mg/L)", values = c("#a6bddb", "#fc8d59", "#d7301f", "#7f0000"))
+dev.off()
+
+
+
+
+
+### Figure 3: beta diversity
+#' Compute jaccard index
+jaccard = function(x, ...)
+	UseMethod("jaccard", x)
+
+#' Compute jaccard for a single experiment
+#' @param x A flume model
+#' @param output Whether to return a data.table (with quantiles) or a matrix (median only)
+#' @param quantiles The quantiles to use if `output = 'data.table'`
+#' @param tsteps The time steps to analyse
+#' @param thresh The number of time steps a species must be present in order to be consdered part of the community
+jaccard.flume = function(x, output = c("data.frame", "matrix"), quantiles = c(0.1, 0.9),
+				   tsteps = 202:231, thresh = floor(0.25 * (max(tsteps) - min(tsteps)))) {
+	output = match.arg(output)
+	snames = attr(x$networks[[1]], "names_sites")
+	species = lapply(x$networks, \(xx) state(xx, "species", history = TRUE)[tsteps])
+	species = lapply(species, \(xx) Reduce(`+`, xx))
+	species = lapply(species, \(xx) 1 * (xx >= thresh))
+	J = lapply(species, jaccard)
+	J = array(unlist(J), c(nrow(J[[1]]), ncol(J[[1]]), length(J)))
+	Jmed = apply(J, c(1,2), median, na.rm = TRUE)
+	rownames(Jmed) = colnames(Jmed) = snames
+
+	if(output == "matrix")
+		return(Jmed)
+	
+	Jlo = apply(J, c(1,2), quantile, quantiles[1], na.rm = TRUE)
+	Jhi = apply(J, c(1,2), quantile, quantiles[2], na.rm = TRUE)
+	rownames(Jlo) = colnames(Jlo) = rownames(Jhi) = colnames(Jhi) = snames
+	Jmed = reshape2::melt(Jmed, varnames = c("reach1", "reach2"), value.name = "Jaccard")
+	Jlo = reshape2::melt(Jlo, varnames = c("reach1", "reach2"), value.name = "Jaccard_lo")
+	Jhi = reshape2::melt(Jhi, varnames = c("reach1", "reach2"), value.name = "Jaccard_hi")
+	J_dt = merge(Jmed, Jlo, by = c("reach1", "reach2"))
+	merge(J_dt, Jhi, by = c("reach1", "reach2"))
+}
+
+#' Compute the jaccard index from a site by species matrix
+#' @param x A site-by-species matrix
+jaccard.matrix = function(x) {
+	site_sim = x %*% t(x)
+	rich = rowSums(x)
+	ra = matrix(rich, nrow=length(rich), ncol=length(rich))
+	J = 1 - (site_sim / (ra + t(ra) - site_sim))
+}
+
+
+jaccard_pcoa = function(f) {
+	rr = readRDS(file.path("ex2_kamp_np_algae/results", f))
+	params = parse_fname(f)
+	J = jaccard(rr, "matrix")
+	res = data.table(ape::pcoa(J)$vectors[,1:3])
+	res$pollution_conc = params$conc
+	res$pollution_Q = params$Q
+	res$pollution_reach = params$node
+	res$reach = rownames(J)
+	res
+}
+
+
+jacc_results = rbindlist(parallel::mclapply(res_files, jaccard_pcoa, mc.cores = 4))
+
+
+
+# duplicate the control across each pollution reach
+res_jacplot = rbindlist(lapply(c(41, 15, 34), \(r) {
+	res = jacc_results[pollution_reach == r | is.na(pollution_reach)]
+	res$network_status = ifelse(res$pollution_conc == 0, "control", "polluted")
+	res$pollution_reach = r
+	res = merge(res, lags[[paste0('r', r)]], by = "reach", all.x = TRUE)
+	res$reach_status = ifelse(is.na(res$lag) | res$lag < 0, "not polluted", "polluted")
+	res
+}))
+
+res_jacplot_subset = res_jacplot[(pollution_conc == 15 | pollution_conc == 0) & (pollution_Q == 50 | pollution_Q == 0)]
+axgr = "#666666aa"
+pdf(width=7, height = 10, file = "ex2_kamp_np_algae/figures/fig_ex2_beta.pdf")
+ggplot(res_jacplot_subset, aes(x = Axis.1, y = Axis.2, colour = reach_status)) + 
+	geom_vline(xintercept = 0, colour = axgr) + geom_hline(yintercept = 0, colour = axgr) + 
+	geom_point() +
+	facet_grid(pollution_reach ~ network_status) + 
+	theme_minimal() + xlab("PCoA Axis 1") + ylab("PCoA Axis 2") + 
+	scale_colour_manual("Reach Status", values = c("#8da0cb", "#fc8d62"))
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+### color nonsense, it is not working well for comparison
+
+# convert PCoA axes to RGB colors
+jaccard2rgb = function(ax1, ax2, ax3) {
+	sc = function(ax) {
+		ax = ax - min(ax)
+		ax / max(ax)
+	}
+	ax1 = sc(ax1)
+	ax2 = sc(ax2)
+	ax3 = sc(ax3)
+	rgb(ax1, ax2, ax3)
+}
+
+
+jmap_pol = res_jacplot[pollution_conc == 15 & pollution_Q == 50 & pollution_reach == 34]
+jmap_pol$col = jaccard2rgb(jmap_pol$Axis.1, jmap_pol$Axis.2, jmap_pol$Axis.3)
+
+jmap_npol = res_jacplot[pollution_conc == 0 & pollution_Q == 0 & pollution_reach == 34]
+jmap_npol$col = jaccard2rgb(jmap_npol$Axis.1, jmap_npol$Axis.2, jmap_npol$Axis.3)
+
+# reorder the rows to match the order in the river network
+jmap_npol = jmap_npol[match(attr(kamp, "names_sites"), jmap_npol$reach)]
+jmap_npol = jmap_npol[match(attr(kamp, "names_sites"), jmap_npol$reach)]
+
+par(mfrow=c(1,2))
+plot(kamp, vertex.color = jmap_pol$col)
+plot(kamp, vertex.color = jmap_npol$col)
+
